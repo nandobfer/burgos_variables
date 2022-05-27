ba9e2ad083f24acc6765919088651a09487ead69
nandobfer <nandobfer@yahoo.com.br>
2022-05-27 17:13:23 -0300

    added init options and yasmin ip

---
+++ __init__.sh
@@ -0,0 +1,5 @@
+# source $HOME/burgos_variables/github.sh
+# source $HOME/burgos_variables/ip_hosts.sh
+
+# add in /etc/profile.d/burgos_variables.sh
+# for script in $HOME/burgos_variables/*.sh; do source $script; done
